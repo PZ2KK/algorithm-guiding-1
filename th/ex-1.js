95804e4551f02a0b3711dea6ae7c8022d8e1eff3
Pom <kasiphit@gmail.com>
2025-04-04 13:24:29 +0700

    feat: make moveZeroes and romanToInt functionable

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,5 +1,14 @@
 const moveZeroes = function (nums) {
-  //Start coding here
+  let left = 0;
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] !== 0) {
+      let temp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = temp;
+      left++
+    }
+  }
+  return nums
 };
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
@@ -7,3 +16,6 @@ console.log(result1); // [1,3,12,0,0]
 
 const result2 = moveZeroes([0]);
 console.log(result2); // [0]
+
+const result3 = moveZeroes([1, 0, 2, 0, 3, 4]);
+console.log(result3); // [ 1, 2, 3, 4, 0, 0 ]
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,6 +1,24 @@
-const romanToInt = function (s) {
-  //Start coding here
+const romanToInt = function (nums) {
+  const romanNumber = {
+    I: 1, V: 5, X: 10, L: 50,
+    C: 100, D: 500, M: 1000
+  }
+
+  let result = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    const current = romanNumber[nums[i]];
+    const next = romanNumber[nums[i + 1]];
+
+    if (next > current) {
+      result -= current;
+    } else {
+      result += current;
+    }
+  }
+  return result;
 };
+
 
 const result1 = romanToInt("III"); // 3
 console.log(result1);
